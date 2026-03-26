c9faf05a22df41eea9e8ead9d491cfd6394cd87a
Aedin Richardson <99768919+Alteoziel@users.noreply.github.com>
2026-03-26 14:46:24 +0000

    Testing Thu Mar 26 2026 14:46:23 GMT+0000 (Coordinated Universal Time)

--- chapter10/ex02/student/rescue/scripts/scripts.js
+++ chapter10/ex02/student/rescue/scripts/scripts.js
@@ -3,3 +3,16 @@
  File Name: scripts.js
  Date: 03/26/2026
  */
+
+ //Hamburger menu function
+function menu() {
+    var navlinks = document.getElementById("nav-links");
+		var menuicon = document.getElementById("icon");
+		if (navlinks.style.display === "block") {
+		    navlinks.style.display = "none";
+				menuicon.style.color = "#2a1f14";
+		} else {
+		    navlinks.style.display = "block";
+				menuicon.style.color = "#f6eee4";
+		}
+}
