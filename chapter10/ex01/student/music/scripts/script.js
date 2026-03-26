89e5defdfe5c2216508e0ea02a764ead4674e39a
Aedin Richardson <99768919+Alteoziel@users.noreply.github.com>
2026-03-26 13:33:48 +0000

    Testing Thu Mar 26 2026 13:33:48 GMT+0000 (Coordinated Universal Time)

--- chapter10/ex01/student/music/scripts/script.js
+++ chapter10/ex01/student/music/scripts/script.js
@@ -3,3 +3,12 @@
  File Name: script.js
  Date: 03/26/2026
 */
+
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+		} else {
+		    menu.style.display = "block";
+		}
+}
