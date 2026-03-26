bfcc7fd75d072f0434b4e54f6a5cd50213a5d7c0
Aedin Richardson <99768919+Alteoziel@users.noreply.github.com>
2026-03-26 15:01:11 +0000

    Testing Thu Mar 26 2026 15:01:11 GMT+0000 (Coordinated Universal Time)

--- chapter10/ex02/student/rescue/scripts/script.js
+++ chapter10/ex02/student/rescue/scripts/script.js
@@ -20,3 +20,23 @@ function menu() {
 				menuicon.style.color = "#f6eee4";
 		}
 }
+
+function ans1() {
+    heading.textContent = "Is it an orphan?";
+    answer.textContent = "If an animal is alone, then it may be, but the best practice would be to stay away in case the parent is still around.";
+}
+
+function ans2() {
+    heading.textContent = "How can I tell if an animal has rabies?";
+    answer.textContent = "Stand far away for a moment and observe the animal. If it is foaming from the mouth or is acting slightly crazy or aggressive, stay back!";
+}
+
+function ans3() {
+    heading.textContent = "I found a baby bird that fell from its nest. Will the parents orphan it if I touch it?";
+    answer.textContent = "Yes, birds can smell the scent of another creature if it has touched their child and will orphan it.";
+}
+
+function ans4() {
+    heading.textContent = "How can I volunteer?";
+    answer.textContent = "Call or text us today using our contact page!";
+}
